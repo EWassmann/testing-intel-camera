00c119bb0b962746e36118ebf48c7822e5b0c939
EWassmann <107703321+EWassmann@users.noreply.github.com>
2022-11-05 16:12:21 -0400

    Add files via upload

---
+++ tetingdist.py
@@ -0,0 +1,117 @@
+###############################################
+
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+def createList(r1, r2):
+    return list(range(r1, r2+1))
+
+
+
+try:
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        # color_frame = frames.get_color_frame()
+        # if not depth_frame or not color_frame:
+        #     continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        # color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        
+        #below pulls the center of the shape and takes the average deapth of the 10x10 pixle area around it  
+        
+        (y,x) = depth_image.shape
+        print(x)
+        print(y)
+        y = y/2
+        x = x/2
+        # if isinstance(x,int):
+        #     pass
+        # else:
+        #     x = x+.5
+        # if isinstance(y,int):
+        #     pass
+        #     print('y passed')
+        # else:
+        #     y = y+.5
+        x = int(x)
+        y = int(y)
+        XL = createList(x-5,x+5)
+        YL = createList(y-5,y+5)
+        zDepthtot = 0
+        avedepth = 0
+        divisor = 0
+        #zDepth = depth_frame.get_distance(int(x),int(y))
+        for xx in XL:
+            for yy in YL:
+                zDepthtot =zDepthtot+ depth_frame.get_distance(int(xx),int(yy))
+                if depth_frame.get_distance(int(xx),int(yy)) != 0:
+                    divisor = divisor +1
+        try:
+            avedepth = zDepthtot/divisor
+            print(avedepth)
+        except:
+            print('object is too close')
+
+        
+        
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        # if depth_colormap_dim != color_colormap_dim:
+        #     resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+        #     images = np.hstack((resized_color_image, depth_colormap))
+        images = depth_colormap
+        # xxx = images.shape
+        # print(xxx)
+     
+
+
+        # Show images
+        cv2.rectangle(images,(x+5,y+5),(x-5,y-5),(0,0,255),2)
+        #cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        cv2.waitKey(1)
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
+
